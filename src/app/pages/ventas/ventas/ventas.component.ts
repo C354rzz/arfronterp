b8bd7d1a708c73340a19d42f2a0954d5e7ebea94
julio armijo <julio.armijo.rdx@gmail.com>
2024-09-28 16:22:22 -0600

    ft-ventas-cotizaciones tbls

--- src/app/pages/ventas/ventas/ventas.component.ts
+++ src/app/pages/ventas/ventas/ventas.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DatatableComponent, ColumnMode } from '@swimlane/ngx-datatable';
 
 @Component({
   selector: 'app-ventas',
@@ -6,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './ventas.component.scss'
 })
 export class VentasComponent implements OnInit {
-  constructor() { } 
+ 
+  ventas: any = [];
+  ventasfilter: any = [];
+
+  loadingIndicator = true;
+
+  constructor (private _route: ActivatedRoute) {
+    fetch('assets/data/ventas/ventas/Ventas.json')
+    .then(response => response.json())
+    .then(data => {
+      this.ventas = data;
+      this.ventasfilter = [...data]
+    })
+    .catch(error => console.error('Error al cargar las Ventas:', error));
+   }
+
   ngOnInit(): void {
     throw new Error('Method not implemented.');
+  }
+
+  @ViewChild(DatatableComponent) table!: DatatableComponent;
+  
+  ColumnMode = ColumnMode;
+
+  updateFilter(event: any) {
+    const val = event.target.value.toLowerCase();
+    const temp = this.ventasfilter.filter(function (d: any) {
+      return d.Contacto.toLowerCase().indexOf(val) !== -1 || !val;
+    });
+    this.ventas = temp;
+    this.table.offset = 0;
   }
 
 }
